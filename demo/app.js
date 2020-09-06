40c104b4bdc6f440d7c244b0ca781652979a87b7
Orest Bida <orest.bida@hotmail.com>
2020-09-06 13:36:48 +0200

    update demo with example of defining new lang

--- demo/app.js
+++ demo/app.js
@@ -1,84 +1,84 @@
 (function(){
 
-    // load plugin
-    loadJS('../src/cookieconsent.js', function(){
+	var auto = true;
+	var lorem_ipsum_short = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+	var lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur';
+	
+	// obtain plugin object
+	var cc = initCookieConsent();
 
-        // obtain plugin object
-        var cc = initCookieConsent();
-
-        cc.run({
-            "cc_autorun" : true, 							// show as soon as possible (without the need to manually call CookieConsent.show() )
-            "cc_delay" : 0,								// specify initial delay after website has loaded
-            "cc_website_name" : "Orest Bida",
-            "cc_website_url" : "orestbida.com",				
-            "cc_enable_verbose" : true,						// if enabled, prints all info/error msgs	
-            "cc_current_lang" : "en",	
-            "cc_auto_language" : false,						// if enabled, overrides cc_current_lang
-            "cc_theme_css" : "../src/cookieconsent.css"		// path to cookieconsent css
-        });
-
-        addEvent(document.getElementById("btn1"), "click", function(){
-            autorun();
-        });
-    
-        addEvent(document.getElementById("btn2"), "click", function(){
-            cc.show(0);                              // specify 0 as delay value
-        });
-    
-        addEvent(document.getElementById("btn3"), "click", function(){
-            cc.hide();                               // hide cookie consent
-        });
-    
-        addEvent(document.getElementById("btn4"), "click", function(){
-            cc.clearCookies();                        
-        });
-    })
-
-
-    //autorun();
-
-    /**
-     * Add event listener to dom object (cross browser function)
-     * @param {Object} elem 
-     * @param {string} event //event type
-     * @param {Object } fn 
-     * @param {boolean} passive
-     */
-    function addEvent(elem, event, fn, passive) {
-        var passive = passive || false;
-
-        if (elem.addEventListener) {
-            passive ? elem.addEventListener(event, fn, { capture: true, passive: true }) : elem.addEventListener(event, fn, false);
-        } else {
-            /**
-             * For old browser, convert "click" and "focus" event to onclick
-             * since the're not always supported
-             */
-            if (event == "click" || event == "focus") {
-                event = "onclick";
-            }
-            elem.attachEvent(event, fn);
-        }
-    }
-
-    function loadJS(url, callback) {
-        var script = document.createElement( "script" )
-        script.type = "text/javascript";
-        if(script.readyState) {  // only required for IE <9
-          script.onreadystatechange = function() {
-            if ( script.readyState === "loaded" || script.readyState === "complete" ) {
-              script.onreadystatechange = null;
-              callback();
-            }
-          };
-        } else {  //Others
-          script.onload = function() {
-            callback();
-          };
-        }
-      
-        script.src = url;
-        document.getElementsByTagName("head")[0].appendChild(script);
+	// init plugin
+	var autorun = function(){
+		cc.run({
+			cc_autorun : auto, 							// show as soon as possible (without the need to manually call CookieConsent.show() )
+			cc_delay : 0,								    // specify initial delay after website has loaded		
+			cc_enable_verbose : true,						// if enabled, prints all info/error msgs	
+			cc_current_lang : 'en',							
+			cc_policy_url : null,                           // specify your own dedicated cookie policy page url
+			cc_auto_language : true,						// if enabled, overrides cc_current_lang
+			cc_theme_css : "../src/cookieconsent.css",		// path to cookieconsent css
+			cc_languages : [
+				{
+					lang : 'it',
+					modal : {
+						cc_title : "Noi usiamo i cookies",
+						cc_description : lorem_ipsum_short,
+						cc_more_text : "Scopri di più",
+						cc_accept_text : "Ho capito"
+					},
+					policy : {
+						ccp_title : "La mia cookie-policy",
+						ccp_save_text : "Salva preference",
+						ccp_blocks : [
+							{
+								title : "Cookie essenziali",
+								description: lorem_ipsum + "<br><br>" + lorem_ipsum
+							},
+							{
+								title : "Cookie di funzionalità",
+								description: lorem_ipsum,
+								switch : {
+									value : 'cc_funzionalita',
+									enabled : true,
+									readonly : true
+								}
+							},
+							{
+								title : "Cookie pubblicitari",
+								description: lorem_ipsum,
+								switch : {
+									value : 'cc_publicita',
+									enabled : false,
+									readonly : false
+								}
+							}
+						]
+					}
+				}
+			]
+		});
 	}
 
+	if(auto)
+		autorun();
+
+	document.getElementById("btn1").addEventListener('click', function(){
+		autorun();
+	});
+
+	document.getElementById("btn2").addEventListener('click', function(){
+		cc.show(0);
+	});
+
+	document.getElementById("btn3").addEventListener('click', function(){
+		cc.hide();
+	});
+
+	document.getElementById("btn4").addEventListener('click', function(){
+		cc.clearCookies();
+	});
+
+	document.getElementById("btn5").addEventListener('click', function(){
+		cc.show_policy(0);  
+	});
 })();
