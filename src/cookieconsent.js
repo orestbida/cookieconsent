629818da2dc8919dedb83b9f633faa7428fc6109
Orest Bida <orest.bida@hotmail.com>
2020-09-15 22:42:58 +0200

    can now add custom tables for cookies

--- src/cookieconsent.js
+++ src/cookieconsent.js
@@ -55,82 +55,7 @@
 		 * Here you can also add additional languages
 		 */
 		var _cc_languages = {
-			"en" : {
-				modal : {
-					cc_title :  "I use cookies",
-					cc_more_text :  "Learn more", 
-					cc_accept_text : "I understand",
-					cc_description :  'My website uses essential cookies necessary for its functioning. By continuing browsing, you consent to my use of cookies and other technologies.',
-				},
-				policy : {
-					ccp_title : "Cookie Policy",
-					ccp_blocks : [
-						{
-							ccb_title : "What are cookies",
-							ccb_description: 'Cookies are very small text files that are stored on your computer when you visit a website. I use cookies to assure the basic functionalities of the website and to enhance your online experience. I use many different types of cookies which you can check on the sections below.'
-						},{
-							ccb_title : "Strictly necessary cookies",
-							ccb_description: 'These cookies are essential for the proper functioning of my website. Without these cookies, the website would not work properly.',
-							ccb_cookies_table : [
-								{
-									ccb_cookie_name: '__Secure-PHPSESSID',
-									ccb_cookie_domain: 'orestbida.com',
-									ccb_cookie_expiration: 'When the browsing session ends',
-									ccb_cookie_description: 'Cookie that stores a random string which represents your current session id' ,
-									ccb_cookie_type: 'Session cookie'
-								},
-								{
-									ccb_cookie_name: '__cfduid',
-									ccb_cookie_domain: 'cloudflare.com',
-									ccb_cookie_expiration: 'When you leave the website',
-									ccb_cookie_description: 'Used to identify web traffic by cloudflare. This cookie does not store any personal information.',
-									ccb_cookie_type: 'Third-party cookie'
-								},
-								{
-									ccb_cookie_name: 'cc_cookie',
-									ccb_cookie_domain: 'orestbida.com',
-									ccb_cookie_expiration: 'After 3 months (Starting from the moment the cookie-consent was accepted)',
-									ccb_cookie_description: 'Used to know whether a visitor has accepted the cookie consent or not.',
-									ccb_cookie_type: 'Permanent cookie'
-								},
-								{
-									ccb_cookie_name: 'cc_level',
-									ccb_cookie_domain: 'orestbida.com',
-									ccb_cookie_expiration: 'After 3 months (Starting from the moment the cookie-consent was accepted)',
-									ccb_cookie_description: 'Used to know the accepted level of cookie consent (E.g.  essential cookie only, full cookie consent ...)',
-									ccb_cookie_type: 'Permanent cookie'
-								}
-							],
-							ccb_switch : {
-								value : 'necessary_cookies',
-								enabled : true,
-								readonly: true
-							}
-						},{
-							ccb_title : "Functionality cookies",
-							ccb_description: 'These cookies are used to provide you with a more personalized experience on my website and to remember choices you make when you browse the website. For example, whether or not you enabled dark-mode on this website.',
-							ccb_switch : {
-								value : 'functionality_cookies',
-								enabled : true,
-								readonly: false
-							},
-							ccb_cookies_table: [
-								{
-									ccb_cookie_name: 'darkmode',
-									ccb_cookie_domain: 'orestbida.com',
-									ccb_cookie_expiration: 'One week after the cookie has been created',
-									ccb_cookie_description: 'Used to remember visitor preferences. If darkmode was enabled, then the next time you visit the website, darkmode will be automatically turned on.' ,
-									ccb_cookie_type: 'Permanent cookie'
-								}
-							]
-						},{
-							ccb_title : "More information",
-							ccb_description: 'For any queries in relation to my policy on cookies and your choices, please contact me.',
-						}
-					],
-					ccp_save_text : "Save preferences"
-				}
-			}
+			// ... define a new langs
 		};
 
 		/**
@@ -229,7 +154,24 @@
 												block_tmp.ccb_title = all_blocks[x]['ccb_title'];
 												block_tmp.ccb_description = all_blocks[x]['ccb_description'];
 
-												// TO DO --> CCB_COOKIE_TABLE
+												/**
+												 * Create cookies-table for this block
+												 */
+												if(all_blocks[x].hasOwnProperty('ccb_cookies_table')){
+													block_tmp.ccb_cookies_table = [];
+								
+													var all_cookies_table_tmp = all_blocks[x]['ccb_cookies_table']
+
+													for(var t=0; t<all_cookies_table_tmp.length; t++){
+														var ccb_cookie_tmp = {};
+														ccb_cookie_tmp.ccb_cookie_name = all_cookies_table_tmp[t]['ccb_cookie_name'];
+														ccb_cookie_tmp.ccb_cookie_domain = all_cookies_table_tmp[t]['ccb_cookie_domain'];
+														ccb_cookie_tmp.ccb_cookie_expiration = all_cookies_table_tmp[t]['ccb_cookie_expiration'];
+														ccb_cookie_tmp.ccb_cookie_description = all_cookies_table_tmp[t]['ccb_cookie_description'];
+														ccb_cookie_tmp.ccb_cookie_type = all_cookies_table_tmp[t]['ccb_cookie_type'];
+														block_tmp.ccb_cookies_table.push(ccb_cookie_tmp);
+													}
+												}
 
 												if(all_blocks[x].hasOwnProperty('ccb_switch')){
 													var ccb_switch = {};
@@ -278,8 +220,25 @@
 									
 									block_tmp.ccb_title = all_blocks[j]['ccb_title'];
 									block_tmp.ccb_description = all_blocks[j]['ccb_description'];
+									
 
 									// TO DO --> CCB_COOKIE_TABLE
+									if(all_blocks[j].hasOwnProperty('ccb_cookies_table')){
+										block_tmp.ccb_cookies_table = [];
+		
+										var all_cookies_table_tmp = all_blocks[j]['ccb_cookies_table']
+
+										for(var t=0; t<all_cookies_table_tmp.length; t++){
+											var ccb_cookie_tmp = {};
+											ccb_cookie_tmp.ccb_cookie_name = all_cookies_table_tmp[t]['ccb_cookie_name'];
+											ccb_cookie_tmp.ccb_cookie_domain = all_cookies_table_tmp[t]['ccb_cookie_domain'];
+											ccb_cookie_tmp.ccb_cookie_expiration = all_cookies_table_tmp[t]['ccb_cookie_expiration'];
+											ccb_cookie_tmp.ccb_cookie_description = all_cookies_table_tmp[t]['ccb_cookie_description'];
+											ccb_cookie_tmp.ccb_cookie_type = all_cookies_table_tmp[t]['ccb_cookie_type'];
+											block_tmp.ccb_cookies_table.push(ccb_cookie_tmp);
+
+										}
+									}
 
 									if(all_blocks[j].hasOwnProperty('ccb_switch')){
 										var ccb_switch = {};
@@ -290,6 +249,7 @@
 									}
 
 									_cc_languages[lang_index].policy.ccp_blocks.push(block_tmp);
+									
 								}
 							}
 						}
@@ -642,8 +602,8 @@
 				block_section.appendChild(block_desc);
 
 				// if cookie table found, generate table for this block
-				if(all_blocks[i].hasOwnProperty('ccb_cookies_table')){
-					
+				if(typeof all_blocks[i].ccb_cookies_table !== 'undefined'){
+
 					// generate cookie-table for this block
 					var block_table = document.createElement('table');
 
@@ -674,6 +634,7 @@
 
 					// create table content
 					for(var n=0; n<all_blocks[i].ccb_cookies_table.length; n++){
+
 						var tr = document.createElement('tr');
 						var td1 = document.createElement('td');
 						var td2 = document.createElement('td');
@@ -943,8 +904,8 @@
 			expires = "; expires=" + date.toUTCString();
 		
 			document.cookie = name + "=" + (value || "") + expires + "; path=/";
-			if(location.protocol == "https")
-				document.cookie = "tagname = test;secure";
+			//if(location.protocol == "https")
+				//document.cookie = "tagname = test;secure";
 
 			_printVerbose("CookieConsent [cookie_consent]: cookie "+ name + "='" + value + "' was set!");
 		}
