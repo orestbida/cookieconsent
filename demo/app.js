65e20fca2236280ef8e59f1bc4b191da44af0634
Orest Bida <orest.bida@hotmail.com>
2020-09-20 14:04:19 +0200

    :fire: simplify demo

--- demo/app.js
+++ demo/app.js
@@ -1,16 +1,12 @@
 (function(){
 
-	var auto = true;
-	var lorem_ipsum_short = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
-	var lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur';
-	
 	// obtain plugin object
 	var cc = initCookieConsent();
 
 	// init plugin
 	var autorun = function(){
 		cc.run({
-			cc_autorun : auto, 							// show as soon as possible (without the need to manually call CookieConsent.show() )
+			cc_autorun : true, 							// show as soon as possible (without the need to manually call CookieConsent.show() )
 			cc_delay : 0,								    // specify initial delay after website has loaded		
 			cc_enable_verbose : true,						// if enabled, prints all info/error msgs	
 			cc_current_lang : 'en',							
@@ -85,8 +81,8 @@
 		});
 	}
 
-	if(auto)
-		autorun();
+	
+	autorun();
 
 	document.getElementById("btn1").addEventListener('click', function(){
 		autorun();
