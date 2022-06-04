865c228d6dc519d34cdf1f0f9878a25b5772f75d
Orest Bida <orest.bida@hotmail.com>
2022-06-04 12:31:55 +0200

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,7 @@ import { terser } from "rollup-plugin-terser";
 import { babel } from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import eslint from '@rollup/plugin-eslint';
-
-import {main, module} from './package.json';
+import pkg from './package.json';
 
 const srcDir = './src';
 const distDir = './dist';
@@ -12,56 +11,52 @@ const input = `${srcDir}/index.js`;
 const productionMode = !process.env.ROLLUP_WATCH;
 const isIE = process.env.BROWSER === 'IE';
 
-const plugins = [
-    eslint({
-        fix: true,
-        include: ['./src/**'],
-        exclude: ['./src/scss/**']
-    }),
-    isIE && babel({
-        exclude:'node_modules/**',
-        babelHelpers: 'bundled',
-        configFile: './babel.config.js'
-    }),
-    productionMode && terser({
-        toplevel: true,
-        format: {
-            quote_style: 1,
-            comments: false
-        },
-        mangle: {
-            properties: {
-                regex: /^_/,
-                keep_quoted: true
-            }
-        },
-        compress: {
-            drop_console: true,
-            passes: 3,
-            pure_funcs: [ '_log']
-        }
-    }),
-];
-
 export default defineConfig(
     [
         {
             input: input,
-            output: {
-                name: 'CookieConsent',
-                file: main,
-                format: 'umd'
-            },
-            plugins: plugins
-        },
-        {
-            input: input,
-            output: {
-                file: module,
-                format: "esm",
-                exports: "named"
-            },
-            plugins: plugins,
+            output: [
+                {
+                    file: pkg.main,
+                    format: 'umd',
+                    name: 'CookieConsent'
+                },
+                {
+                    file: pkg.module,
+                    format: "esm",
+                    exports: "named"
+                }
+            ],
+            plugins: [
+                eslint({
+                    fix: true,
+                    include: ['./src/**'],
+                    exclude: ['./src/scss/**']
+                }),
+                isIE && babel({
+                    exclude:'node_modules/**',
+                    babelHelpers: 'bundled',
+                    configFile: './babel.config.js'
+                }),
+                productionMode && terser({
+                    toplevel: true,
+                    format: {
+                        quote_style: 1,
+                        comments: false
+                    },
+                    mangle: {
+                        properties: {
+                            regex: /^_/,
+                            keep_quoted: true
+                        }
+                    },
+                    compress: {
+                        drop_console: true,
+                        passes: 3,
+                        pure_funcs: [ '_log']
+                    }
+                }),
+            ]
         },
         {
             input: `${srcDir}/scss/index.scss`,
