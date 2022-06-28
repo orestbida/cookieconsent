61a8655d23f3323f0376c30709de7b66655f94c9
Andreas Fuchs <info@anfuchs.de>
2022-06-28 18:41:35 +0200

    style changes

--- demo/demo_iframemanager/cookieconsent-init.js
+++ demo/demo_iframemanager/cookieconsent-init.js
@@ -1,5 +1,4 @@
-var LOREM_IPSUM =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+var LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
 
 // obtain iframemanager object
 var manager = iframemanager();
@@ -9,21 +8,21 @@ var cc = initCookieConsent();
 
 // Configure with youtube embed
 manager.run({
-    currLang: "en",
+    currLang: 'en',
     services: {
         youtube: {
-            embedUrl: "https://www.youtube-nocookie.com/embed/{data-id}",
-            thumbnailUrl: "https://i3.ytimg.com/vi/{data-id}/hqdefault.jpg",
+            embedUrl: 'https://www.youtube-nocookie.com/embed/{data-id}',
+            thumbnailUrl: 'https://i3.ytimg.com/vi/{data-id}/hqdefault.jpg',
             iframe: {
-                allow: "accelerometer; encrypted-media; gyroscope; picture-in-picture; fullscreen;",
+                allow: 'accelerometer; encrypted-media; gyroscope; picture-in-picture; fullscreen;',
             },
             cookie: {
-                name: "cc_youtube",
+                name: 'cc_youtube',
             },
             languages: {
                 en: {
                     notice: 'This content is hosted by a third party. By showing the external content you accept the <a rel="noreferrer" href="https://www.youtube.com/t/terms" title="Terms and conditions" target="_blank">terms and conditions</a> of youtube.com.',
-                    loadBtn: "Load video",
+                    loadBtn: 'Load video',
                     loadAllBtn: "Don't ask again",
                 },
             },
@@ -33,9 +32,9 @@ manager.run({
 
 // run plugin with config object
 cc.run({
-    current_lang: "en",
+    current_lang: 'en',
     autoclear_cookies: true, // default: false
-    cookie_name: "cc_cookie_demo2", // default: 'cc_cookie'
+    cookie_name: 'cc_cookie_demo2', // default: 'cc_cookie'
     cookie_expiration: 365, // default: 182
     // page_scripts: false,                         // default: false
     force_consent: true, // default: false
@@ -53,41 +52,41 @@ cc.run({
 
     gui_options: {
         consent_modal: {
-            layout: "cloud", // box,cloud,bar
-            position: "bottom center", // bottom,middle,top + left,right,center
-            transition: "slide", // zoom,slide
+            layout: 'cloud', // box,cloud,bar
+            position: 'bottom center', // bottom,middle,top + left,right,center
+            transition: 'slide', // zoom,slide
         },
         settings_modal: {
-            layout: "bar", // box,bar
-            position: "left", // right,left (available only if bar layout selected)
-            transition: "slide", // zoom,slide
+            layout: 'bar', // box,bar
+            position: 'left', // right,left (available only if bar layout selected)
+            transition: 'slide', // zoom,slide
         },
     },
 
     onFirstAction: function () {
-        console.log("onFirstAction fired");
+        console.log('onFirstAction fired');
     },
 
     onAccept: function () {
-        console.log("onAccept fired!");
+        console.log('onAccept fired!');
 
         // If analytics category is disabled => load all iframes automatically
-        if (cc.allowedCategory("analytics")) {
-            console.log("iframemanager: loading all iframes");
-            manager.acceptService("all");
+        if (cc.allowedCategory('analytics')) {
+            console.log('iframemanager: loading all iframes');
+            manager.acceptService('all');
         }
     },
 
     onChange: function (cookie, changed_preferences) {
-        console.log("onChange fired!");
+        console.log('onChange fired!');
 
         // If analytics category is disabled => ask for permission to load iframes
-        if (!cc.allowedCategory("analytics")) {
-            console.log("iframemanager: disabling all iframes");
-            manager.rejectService("all");
+        if (!cc.allowedCategory('analytics')) {
+            console.log('iframemanager: disabling all iframes');
+            manager.rejectService('all');
         } else {
-            console.log("iframemanager: loading all iframes");
-            manager.acceptService("all");
+            console.log('iframemanager: loading all iframes');
+            manager.acceptService('all');
         }
     },
 
@@ -95,83 +94,68 @@ cc.run({
         en: {
             consent_modal: {
                 title: "Hello traveller, it's cookie time!",
-                description:
-                    'Our website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <a href="#privacy-policy" class="cc-link">Privacy policy</a>',
+                description: 'Our website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <a href="#privacy-policy" class="cc-link">Privacy policy</a>',
                 primary_btn: {
-                    text: "Accept all",
-                    role: "accept_all", //'accept_selected' or 'accept_all'
+                    text: 'Accept all',
+                    role: 'accept_all', //'accept_selected' or 'accept_all'
                 },
                 secondary_btn: {
-                    text: "Preferences",
-                    role: "settings", //'settings' or 'accept_necessary'
+                    text: 'Preferences',
+                    role: 'settings', //'settings' or 'accept_necessary'
                 },
-                revision_message:
-                    "<br><br> Dear user, terms and conditions have changed since the last time you visisted!",
+                revision_message: '<br><br> Dear user, terms and conditions have changed since the last time you visisted!',
             },
             settings_modal: {
-                title: "Cookie settings",
-                save_settings_btn: "Save current selection",
-                accept_all_btn: "Accept all",
-                reject_all_btn: "Reject all",
-                close_btn_label: "Close",
-                cookie_table_headers: [
-                    { col1: "Name" },
-                    { col2: "Domain" },
-                    { col3: "Expiration" },
-                ],
+                title: 'Cookie settings',
+                save_settings_btn: 'Save current selection',
+                accept_all_btn: 'Accept all',
+                reject_all_btn: 'Reject all',
+                close_btn_label: 'Close',
+                cookie_table_headers: [{ col1: 'Name' }, { col2: 'Domain' }, { col3: 'Expiration' }],
                 blocks: [
                     {
-                        title: "Cookie usage",
-                        description:
-                            LOREM_IPSUM +
-                            ' <a href="#" class="cc-link">Privacy Policy</a>.',
+                        title: 'Cookie usage',
+                        description: LOREM_IPSUM + ' <a href="#" class="cc-link">Privacy Policy</a>.',
                     },
                     {
-                        title: "Strictly necessary cookies",
-                        description:
-                            LOREM_IPSUM +
-                            LOREM_IPSUM +
-                            "<br><br>" +
-                            LOREM_IPSUM +
-                            LOREM_IPSUM,
+                        title: 'Strictly necessary cookies',
+                        description: LOREM_IPSUM + LOREM_IPSUM + '<br><br>' + LOREM_IPSUM + LOREM_IPSUM,
                         toggle: {
-                            value: "necessary",
+                            value: 'necessary',
                             enabled: true,
                             readonly: true, //cookie categories with readonly=true are all treated as "necessary cookies"
                         },
                     },
                     {
-                        title: "Analytics & Performance cookies",
+                        title: 'Analytics & Performance cookies',
                         description: LOREM_IPSUM,
                         toggle: {
-                            value: "analytics",
+                            value: 'analytics',
                             enabled: false,
                             readonly: false,
                         },
                         cookie_table: [
                             {
-                                col1: "^_ga",
-                                col2: "yourdomain.com",
-                                col3: "description ...",
+                                col1: '^_ga',
+                                col2: 'yourdomain.com',
+                                col3: 'description ...',
                                 is_regex: true,
                             },
                             {
-                                col1: "_gid",
-                                col2: "yourdomain.com",
-                                col3: "description ...",
+                                col1: '_gid',
+                                col2: 'yourdomain.com',
+                                col3: 'description ...',
                             },
                             {
-                                col1: "cc_youtube",
-                                col2: "yourdomain.com",
-                                col3: "Cookie set by iframemanager",
+                                col1: 'cc_youtube',
+                                col2: 'yourdomain.com',
+                                col3: 'Cookie set by iframemanager',
                             },
                         ],
                     },
                     {
-                        title: "More information",
-                        description:
-                            LOREM_IPSUM +
-                            ' <a class="cc-link" href="https://orestbida.com/contact/">Contact me</a>.',
+                        title: 'More information',
+                        description: LOREM_IPSUM + ' <a class="cc-link" href="https://orestbida.com/contact/">Contact me</a>.',
                     },
                 ],
             },
