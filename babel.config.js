3eb0b82e757c12053aebec1c0396069ca5234c9a
Orest Bida <dev@orestbida.com>
2022-08-02 00:18:34 +0200

    Update babel config: cleanup

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,7 @@
 module.exports = {
     presets: [
         [
-            '@babel/preset-env',
-            {
-                targets: {
-                    node: 'current'
-                }
-            }
+            '@babel/preset-env'
         ]
     ]
 }
