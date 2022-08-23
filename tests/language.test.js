7e13548d8d1e8f26c112eb8927b1f4f00251a16e
Orest Bida <dev@orestbida.com>
2022-08-23 15:36:01 +0200

    Update tests: added more tests for language utilities

--- tests/language.test.js
+++ tests/language.test.js
@@ -2,7 +2,9 @@ import { globalObj } from '../src/core/global';
 import {
     _getBrowserLanguageCode,
     _resolveCurrentLanguageCode,
-    _getValidLanguageCode
+    _getCurrentLanguageCode,
+    _getDocumentLanguageCode,
+    _validLanguageCode
 } from '../src/utils/language'
 
 describe('Test language utils', () => {
@@ -11,9 +13,48 @@ describe('Test language utils', () => {
         Object.defineProperty(global.navigator, 'language', {value: 'it-IT', configurable: true});
     })
 
+    it("Should return true when translation is defined", () => {
+        globalObj._state._allTranslations = {
+            en: {}
+        }
+        expect(_validLanguageCode('en')).toBe(true);
+    });
+
+    it("Should return false when translation is not defined", () => {
+        globalObj._state._allTranslations = {
+            en: {}
+        }
+        expect(_validLanguageCode('it')).toBe(false);
+    });
+
+    it("Should return current language", () => {
+        globalObj._state._currentLanguageCode = 'de';
+        expect(_getCurrentLanguageCode()).toBe('de');
+    });
+
     it("Should return the browser's language code", () => {
         const language = _getBrowserLanguageCode();
         expect(language).toBe('it');
+    });
+
+    it("Should return the document's 'lang' attribute", () => {
+        document.documentElement.lang = 'es';
+        const language = _getDocumentLanguageCode();
+        expect(language).toBe('es');
+    });
+
+    it("Should fallback to 'default' language when autoDetect language is not defined", () => {
+        document.documentElement.lang = 'es';
+        globalObj._state._allTranslations = {
+            en: {}
+        }
+        globalObj._state._userConfig = {};
+        globalObj._state._currentLanguageCode = '';
+        globalObj._state._userConfig.language = {};
+        globalObj._state._userConfig.language.default = 'en';
+        globalObj._state._userConfig.language.autoDetect = 'document';
+        const language = _resolveCurrentLanguageCode();
+        expect(language).toBe('en');
     });
 
     it("Should use the browser's language", () => {
@@ -22,22 +63,10 @@ describe('Test language utils', () => {
             en: {}
         }
         globalObj._state._userConfig = {};
+        globalObj._state._currentLanguageCode = '';
         globalObj._state._userConfig.language = {};
         globalObj._state._userConfig.language.default = 'en';
         globalObj._state._userConfig.language.autoDetect = 'browser';
         expect(_resolveCurrentLanguageCode()).toBe('it');
-    })
-
-    it('Should return the first valid language', () => {
-        globalObj._state._allTranslations = {
-            it: {}
-        }
-        globalObj._state._userConfig = {};
-        globalObj._state._userConfig.language = {};
-        globalObj._state._userConfig.language.autoDetect = 'browser';
-        globalObj._state._currentLanguageCode = 'en';
-        globalObj._state._userConfig.language.default = 'en';
-        const currentLanguage = _getValidLanguageCode('en');
-        expect(currentLanguage).toBe('it');
     })
 });
