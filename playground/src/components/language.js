7d11d584b6cc8e9cf7ca30da58c0cd7d7e03e0aa
Orest Bida <dev@orestbida.com>
2023-05-01 02:42:56 +0200

    Update playground: more tweaks/fixes

--- playground/src/components/language.js
+++ playground/src/components/language.js
@@ -1,28 +1,46 @@
+import { defaultFullConfig } from './defaultConfig';
 import { defaultConfig } from './defaultConfig';
 import { getState, saveState } from './stateManager';
 import { addEvent, customEvents, getById, onEvent } from './utils';
 
 const autoDetectModes = ['document', 'browser'];
-const currentActiveLanguageSpan = getById('current-language');
+const currentActiveLanguageSpan = getById('current-active-language');
 
 /** @type {HTMLInputElement} **/ const autoDetectCheckbox = getById('auto-language');
 /** @type {HTMLInputElement} **/ const detectedLanguageSpan = getById('detected-language');
 /** @type {HTMLInputElement} **/ const translationFoundSpan = getById('translation-found');
 /** @type {HTMLSelectElement} **/ const autoDetectModeSelect = getById('autodetect-mode');
+/** @type {HTMLSelectElement} **/ export const defaultLanguageSelect = getById('default-language');
 
 const state = getState();
 const language = state.cookieConsentConfig.language;
-const allLanguages = Object.keys(language.translations);
-
-const browserLanguage = getBrowserLanguage();
+const allLanguages = Object.keys(defaultFullConfig.language.translations);
 
 let autoDetectEnabled = autoDetectModes.includes(language.autoDetect);
+const detectedLanguage = autoDetectEnabled ? detectLanguage(language.autoDetect) : '';
+
+/** [TODO: FIX ABOMINATION] */
+const currLang = state.enabledTranslations.includes(detectedLanguage) ? detectedLanguage : state.currLanguage || state.cookieConsentConfig.language.default;
+
+console.log('autoDetectEnabled', autoDetectEnabled)
+console.log('detectedLanguage', detectedLanguage);
+
+toggleAutoDetectCheckbox(autoDetectEnabled)
 
 if(autoDetectEnabled) {
-    setAutoDetectMode(language.autoDetect || defaultConfig.language.autoDetect);
+    setAutoDetectMode(language.autoDetect);
+    setAutoDetectLanguage(detectedLanguage)
 } else {
     setAutoDetectMode('');
 }
+console.log('currLang', currLang)
+updateDefaultLanguageOptions(state.enabledTranslations);
+setDefaultLanguage(language.default);
+setActiveLanguage(currLang);
+
+onEvent(customEvents._INIT, () => {
+    window.CookieConsent.setLanguage(getState().currLanguage);
+});
 
 addEvent(autoDetectCheckbox, 'change', () => {
 
@@ -34,30 +52,30 @@ addEvent(autoDetectCheckbox, 'change', () => {
 
         language.autoDetect ||= defaultConfig.language.autoDetect;
 
-        if(allLanguages.includes(browserLanguage)){
-            state.currLanguage = browserLanguage;
-            // setActiveLanguage(browserLanguage);
+        const detectedLang = detectLanguage(language.autoDetect);
+
+        console.log('detected', detectedLang, state.enabledTranslations);
+
+        if(state.enabledTranslations.includes(detectedLang)){
+            state.currLanguage = detectedLang;
+            setActiveLanguage(detectedLang);
         }
 
+        setAutoDetectLanguage(detectedLang);
         setAutoDetectMode(language.autoDetect);
 
-        setAutoDetectLanguage(browserLanguage);
     }else {
         language.autoDetect = undefined;
-        state.currLanguage = language.default;
+
+        if(!state.enabledTranslations.includes(state.currLanguage))
+            state.currLanguage = language.default;
 
         setAutoDetectLanguage(false);
         setAutoDetectMode('');
-        // setActiveLanguage(language.default);
+        setActiveLanguage(state.currLanguage);
     }
 
     saveState(state);
-
-    window.CookieConsent
-        .setLanguage(state.currLanguage)
-        .then(()=>{
-            window.CookieConsent.show(true);
-        });
 });
 
 addEvent(autoDetectModeSelect, 'change', () => {
@@ -66,22 +84,42 @@ addEvent(autoDetectModeSelect, 'change', () => {
     if(autoDetectModes.includes(mode)) {
         setAutoDetectMode(mode);
         const state = getState();
+
+        const detectedLanguage = detectLanguage(mode);
+
+        if(state.enabledTranslations.includes(detectedLanguage)){
+            state.currLanguage = detectedLanguage;
+            setActiveLanguage(detectedLanguage);
+        }
+
         state.cookieConsentConfig.language.autoDetect = mode;
+        saveState(state);
+    }
+});
+
+addEvent(defaultLanguageSelect, 'change', () => {
+    const lang = defaultLanguageSelect.value;
+    const state = getState();
+
+    if(state.enabledTranslations.includes(lang)){
+        state.cookieConsentConfig.language.default = lang;
+        state.currLanguage = lang;
+
+        /** [TODO: FIX ABOMINATION] */
+        (!state.cookieConsentConfig.language.autoDetect || !state.enabledTranslations.includes(detectLanguage(state.cookieConsentConfig.language.autoDetect))) && setActiveLanguage(lang, state);
         saveState(state);
     }
 });
 
 onEvent(customEvents._RESET, () => {
     const language = defaultConfig.language;
+    const defaultLang = defaultConfig.language.default;
     autoDetectEnabled = autoDetectModes.includes(language.autoDetect);
-
-    const currentLanguage = autoDetectEnabled
-        ? browserLanguage
-        : language.default;
-
-    // setActiveLanguage(currentLanguage);
-
+    setDefaultLanguage(defaultLang);
     setAutoDetectMode(language.autoDetect);
+    setAutoDetectLanguage(defaultLang)
+    setActiveLanguage(defaultLang);
+    toggleAutoDetectCheckbox(true);
 });
 
 
@@ -93,13 +131,32 @@ function getDocumentLanguage(){
     return document.documentElement.lang.slice(0,2);
 }
 
-// /**
-//  * @param {string} languageCode
-//  */
-// function setActiveLanguage(languageCode){
-//     if(allLanguages.includes(languageCode))
-//         currentActiveLanguageSpan.textContent = languageCode;
-// }
+/**
+ * @param {string} mode
+ */
+export function detectLanguage(mode) {
+
+    if(mode === 'browser')
+        return getBrowserLanguage();
+    else if(mode === 'document')
+        return getDocumentLanguage()
+
+    return '';
+}
+
+/**
+ * @param {string} languageCode
+ */
+export function setActiveLanguage(languageCode, newState){
+
+    const state = newState || getState();
+
+    const currLanguage = state.enabledTranslations.includes(languageCode)
+        ? languageCode
+        : '-';
+
+    currentActiveLanguageSpan.textContent = currLanguage;
+}
 
 /**
  * @param {string|boolean} languageCode
@@ -108,25 +165,26 @@ function setAutoDetectLanguage(languageCode){
 
     if(languageCode && typeof languageCode === 'string'){
 
-        autoDetectEnabled = true;
-        autoDetectCheckbox.checked = true;
-        detectedLanguageSpan.textContent = languageCode;
+        const validLanguage = getState().enabledTranslations.includes(languageCode);
 
-        translationFoundSpan.textContent = allLanguages.includes(languageCode)
+        translationFoundSpan.textContent = validLanguage
             ? 'true'
             : 'false';
 
-        // setActiveLanguage(languageCode);
+        detectedLanguageSpan.textContent = languageCode;
 
         return;
     }
 
-    if(autoDetectEnabled){
-        autoDetectEnabled = false;
-        autoDetectCheckbox.checked = false;
-        detectedLanguageSpan.textContent = '-';
-        translationFoundSpan.textContent = '-';
-    }
+    detectedLanguageSpan.textContent = '-';
+    translationFoundSpan.textContent = '-';
+}
+
+/**
+ * @param {boolean} enable
+ */
+function toggleAutoDetectCheckbox(enable) {
+    autoDetectCheckbox.checked = enable;
 }
 
 /**
@@ -140,11 +198,35 @@ function setAutoDetectMode(mode) {
         autoDetectModeSelect.value = mode;
     }
 
-    const detectedLanguage = mode === 'browser'
-        ? getBrowserLanguage()
-        : getDocumentLanguage();
-
-    setAutoDetectLanguage(validMode ? detectedLanguage : false);
-
     autoDetectModeSelect.disabled = !validMode;
+}
+
+/**
+ * @param {string} lang
+ */
+export function setDefaultLanguage(lang) {
+
+    if(!lang)
+        defaultLanguageSelect.value = 'disabled';
+
+    if(allLanguages.includes(lang))
+        defaultLanguageSelect.value = lang;
+}
+
+/**
+ * @param {string[]} languages
+ */
+export function updateDefaultLanguageOptions(languages) {
+
+    /**
+     * @type {HTMLOptionElement[]}
+     */
+    const options = defaultLanguageSelect.children;
+
+    for(const option of options) {
+
+        option.style.display = languages.includes(option.value)
+            ? ''
+            : 'none';
+    }
 }
--- playground/src/components/translations.js
+++ playground/src/components/translations.js
@@ -1,13 +1,23 @@
+import { updateDefaultLanguageOptions, defaultLanguageSelect, setDefaultLanguage, setActiveLanguage, detectLanguage } from './language';
 import { defaultState, getState, saveState } from './stateManager';
-import { addEvent, customEvents, onEvent } from './utils';
+import { addEvent, customEvents, getById, onEvent } from './utils';
 
 /**
  * @type {NodeListOf<HTMLInputElement>}
  */
 const translationInputs = document.querySelectorAll('input[name="language-code"]');
+const translationsSection = getById('translations-section');
 
+/**
+ * @type {HTMLButtonElement}
+ */
+const editTranslationsBtn = getById('edit-translations-btn');
+const editTranslationsBtnText = editTranslationsBtn.textContent;
 
-toggleTranslations(getState().enabledTranslations);
+const enabledTranslations = getState().enabledTranslations;
+
+toggleTranslations(enabledTranslations);
+toggleMissingTranslationError(enabledTranslations);
 
 translationInputs.forEach(input => {
     addEvent(input, 'change', () => {
@@ -25,6 +35,25 @@ translationInputs.forEach(input => {
             languageFound && (state.enabledTranslations = state.enabledTranslations.filter(language => language !== languageCode));
         }
 
+        toggleMissingTranslationError(state.enabledTranslations)
+        updateDefaultLanguageOptions(state.enabledTranslations);
+
+        if(!state.enabledTranslations.includes(defaultLanguageSelect.value)) {
+            const currentDefaultLang = state.enabledTranslations[0] || '';
+            setDefaultLanguage(currentDefaultLang);
+            state.cookieConsentConfig.language.default = currentDefaultLang;
+
+            state.currLanguage = currentDefaultLang;
+            currentDefaultLang && window.CookieConsent.setLanguage(currentDefaultLang);
+        }
+
+        /** [TODO: FIX ABOMINATION] */
+        if(state.cookieConsentConfig.language.autoDetect && state.enabledTranslations.includes(detectLanguage(state.cookieConsentConfig.language.autoDetect))) {
+            setActiveLanguage(detectLanguage(state.cookieConsentConfig.language.autoDetect), state);
+        } else {
+            setActiveLanguage(state.cookieConsentConfig.language.default, state, );
+        }
+
         saveState(state);
     });
 });
@@ -40,4 +69,22 @@ function toggleTranslations(enabledTranslations) {
     for(const input of translationInputs) {
         input.checked = enabledTranslations.includes(input.value);
     }
+}
+
+/**
+ * @param {string[]} enabledTranslations
+ */
+function toggleMissingTranslationError(enabledTranslations) {
+
+    const error = enabledTranslations.length === 0;
+
+    translationsSection.classList[error
+        ? 'add'
+        : 'remove'
+    ]('no-translations-error');
+
+    editTranslationsBtn.disabled = error;
+    editTranslationsBtn.textContent = error
+        ? 'Translation required'
+        : editTranslationsBtnText;
 }
