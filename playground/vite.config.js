b3a0d7c7dd1b7cad4198852b3e349db20efad9e5
Orest Bida <dev@orestbida.com>
2023-05-04 19:25:09 +0200

    Update vite config: use terser instead of esbuild

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -3,8 +3,28 @@ import { ViteMinifyPlugin } from 'vite-plugin-minify'
 import { ShikiHighlight } from './plugins/shiki-highlighter'
 
 export default defineConfig({
+
     plugins: [
         await ShikiHighlight({theme: 'github-dark'}),
         ViteMinifyPlugin({})
-    ]
+    ],
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            toplevel: true,
+            format: {
+                quote_style: 1,
+                comments: /^!/
+            },
+            mangle: {
+                properties: {
+                    regex: /^_/,
+                    keep_quoted: true
+                }
+            },
+            compress: {
+                drop_console: true
+            }
+        }
+    },
 })
