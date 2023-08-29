9f6316591d2fe2c06e8514a5d821107d5ee670cd
Orest Bida <dev@orestbida.com>
2023-08-29 18:55:31 +0200

    Chore: rebuild dist files

--- dist/core/cookieconsent-core.esm.js
+++ dist/core/cookieconsent-core.esm.js
@@ -1,6 +1,6 @@
 /*!
 * CookieConsent 3.0.0-rc.16
-* https://github.com/orestbida/cookieconsent
+* https://github.com/orestbida/cookieconsent/tree/v3.0-beta
 * Author Orest Bida
 * Released under the MIT License
 */
