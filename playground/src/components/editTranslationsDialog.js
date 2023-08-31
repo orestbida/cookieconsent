8d91d07ab8693bcbb966dee89be2b20a5e8119fa
Orest Bida <dev@orestbida.com>
2023-08-31 23:06:19 +0200

    Feat: add missing translation fields

--- playground/src/components/editTranslationsDialog.js
+++ playground/src/components/editTranslationsDialog.js
@@ -5,7 +5,11 @@ import { getById, addEvent } from './utils';
 import { getState, defaultState } from './stateManager';
 
 const container = getById('edit-translations-dialog');
-const translationsBtns = container.querySelector('.translation-btns');
+
+/**
+ * @type {HTMLSelectElement}
+ */
+const selectEditorTranslation = getById('editor-language-codes');
 
 const dialog = new A11yDialog(container);
 
@@ -22,24 +26,47 @@ const pmCloseIcon = getById('pm-close-icon-label');
 const pmAcceptBtn = getById('pm-accept-btn');
 const pmRejectBtn = getById('pm-reject-btn');
 const pmSaveBtn = getById('pm-save-btn');
+const pmServiceConterLabel = getById('pm-service-counter-label');
 
 /**
  * @type {HTMLButtonElement}
  */
 const editBtn = getById('edit-translations-btn');
 
+
+/**
+ * @type {HTMLButtonElement}
+ */
+const addSectionBtn = getById('add-section-btn');
+const pmSections = getById('pm-sections');
+
 let lastEnabledTranslations = [];
 let selectedTranslation = '';
 
 /**
+ * @type {typeof defaultState}
+ */
+let state = null;
+
+/**
  * Show dialog on click
  */
-addEvent(editBtn, 'click', function () {
+addEvent(editBtn, 'click', () => {
     if(editBtn.disabled)
         return;
 
+    /**
+     * Retrieve the current state
+     */
+    state = getState();
+
     if(updateEditor() !== false)
         dialog.show();
+});
+
+addEvent(addSectionBtn, 'click', () => {
+    const section = createSection();
+    appendSection(section);
 });
 
 /**
@@ -107,11 +134,21 @@ function updateEditorFields(state) {
     pmAcceptBtn.value = preferencesModal.acceptAllBtn || '';
     pmRejectBtn.value = preferencesModal.acceptNecessaryBtn || '';
     pmSaveBtn.value = preferencesModal.savePreferencesBtn || '';
+    pmServiceConterLabel.value = preferencesModal.serviceCounterLabel || '';
+
 
     /**
-     * Preferences modal fields
-     * [TODO]
+     * Update sections
      */
+    const sections = preferencesModal.sections || [];
+    const newSections = [];
+
+    for(const section of sections) {
+        if(state._enabledCategories.includes(section.linkedCategory))
+            newSections.push(createSection(section));
+    }
+
+    pmSections.replaceChildren(...newSections);
 }
 
 /**
@@ -120,22 +157,22 @@ function updateEditorFields(state) {
  */
 function updateAvailableTranslations(enabledTranslations, state) {
 
-    const fragment = document.createDocumentFragment();
+    const options = [];
 
     for(const translation of enabledTranslations) {
-        const btn = document.createElement('button');
-        btn.textContent = translation;
-        btn.className = `translation-btn--${translation}`;
-        fragment.appendChild(btn);
-
-        addEvent(btn, 'click', () => {
-            setActiveTranslation(translation);
-            updateEditorFields(state);
-        });
+        const option = document.createElement('option');
+        option.value = translation;
+        option.textContent = translation;
+        option.className = `translation-btn--${translation}`;
+        options.push(option);
     }
 
-    translationsBtns.textContent = '';
-    translationsBtns.appendChild(fragment);
+    selectEditorTranslation.addEventListener('change', () => {
+        setActiveTranslation(selectEditorTranslation.value);
+        updateEditorFields(state);
+    });
+
+    selectEditorTranslation.replaceChildren(...options);
 }
 
 /**
@@ -143,14 +180,78 @@ function updateAvailableTranslations(enabledTranslations, state) {
  */
 function setActiveTranslation(translation) {
     selectedTranslation = translation;
-    translationsBtns.setAttribute('data-current-translation', translation);
+    selectEditorTranslation.setAttribute('data-current-translation', translation);
 }
 
 /**
- *
  * @param {string[]} arr1
  * @param {string[]} arr2
  */
 function equalArrays(arr1, arr2) {
     return JSON.stringify(arr1.sort()) === JSON.stringify(arr2.sort());
+}
+
+/**
+ * @param {HTMLDivElement} sectionDiv
+ */
+function appendSection(sectionDiv) {
+    pmSections.appendChild(sectionDiv);
+}
+
+/**
+ * @param {{
+ *  title: string,
+ *  description: string,
+ *  linkedCategory: string
+ * }} [sectionData]
+ *
+ * @returns {HTMLLIElement}
+ */
+function createSection(sectionData) {
+
+    const section = document.createElement('li');
+    section.className = 'pm-section';
+
+    const title = document.createElement('textarea');
+    title.value = sectionData?.title || '';
+    title.placeholder = 'Title';
+    title.className = 'pm-section-title';
+    section.appendChild(title);
+
+    const description = document.createElement('textarea');
+    description.value = sectionData?.description || '';
+    description.placeholder = 'Description';
+    description.className = 'pm-section-description';
+    section.appendChild(description);
+
+    const select = document.createElement('select');
+    select.className = 'pm-section-linked-category';
+
+    const linkedCategory = sectionData?.linkedCategory;
+    const categories = ['disabled', ...state._enabledCategories];
+
+    for(const category of categories) {
+        const option = document.createElement('option');
+        option.value = category;
+        option.textContent = category;
+
+        if(category === linkedCategory && state._enabledCategories.includes(linkedCategory)){
+            option.selected = true;
+            option.textContent = linkedCategory;
+        } else if(category === 'disabled'){
+            option.selected = true;
+            option.textContent = 'No linked category';
+        }
+
+        select.appendChild(option);
+    }
+    section.appendChild(select);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.className = 'pm-section-delete-btn';
+    addEvent(deleteBtn, 'click', () => section.remove());
+    section.appendChild(deleteBtn);
+
+    return section;
 }
