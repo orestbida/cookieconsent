b0aa94422ffd32793a5a14422b7fd6fa436c1ad7
Orest Bida <dev@orestbida.com>
2024-01-19 01:09:28 +0100

    fix: astro build fails

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,6 +1,17 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
+import { buildTranslationsExportFile } from './src/modules/serverUtils';
+
+buildTranslationsExportFile();
 
 export default defineConfig({
-  integrations: [compress()]
+  integrations: [compress(), {
+    name: 'build-languages',
+    hooks: {
+      "astro:build:start": async () => {
+        buildTranslationsExportFile();
+      }
+    }
+  }],
+
 });
