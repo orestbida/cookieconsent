e87507282de837e142b666aef205f8a7f9fddd37
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-08-09 11:39:01 -0400

    Revert setCookie changes

--- tests/config/mocks-utils.js
+++ tests/config/mocks-utils.js
@@ -1,14 +1,10 @@
 export const botUserAgent = 'Mozilla/5.0 (Linux; Android 5.0; SM-G920A) AppleWebKit (KHTML, like Gecko) Chrome Mobile Safari (compatible; AdsBot-Google-Mobile; +http://www.google.com/mobile/adsbot.html)';
 
-export const setCookie = (name, value, days = 1, domain = undefined) => {
+export const setCookie = (name, value, days = 1) => {
     const date = new Date();
     date.setTime(date.getTime() + (days*24*60*60*1000));
     const expires = '; expires=' + date.toUTCString();
-    let cookie = name + '=' + value + expires + '; path=/';
-    if (domain) {
-        cookie += '; domain=' + domain;
-    }
-    document.cookie = cookie;
+    document.cookie = name + '=' + value + expires + '; path=/';
 }
 
 export function htmlHasClass(className) {
